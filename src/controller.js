6ba2df7a98e43e2e662df0ff33cb292c6fc24b25
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-05 00:25:11 +0530

    added control timer functionality

--- src/controller.js
+++ src/controller.js
@@ -1,14 +1,25 @@
 import { quizCompetitionstate } from "./questions.js";
 import questionView from "./views/questionView.js";
+import questionNoView from "./views/questionNoView.js";
+import timerView from "./views/timerView.js";
 
+const { questions } = quizCompetitionstate;
 const controlQuestions = function () {
-  const { questions } = quizCompetitionstate;
   console.log(questions);
   questionView.render(questions);
 };
 
+const controlQuestionNumber = function () {
+  questionNoView.render(questions);
+};
+
+const controltimer = function () {
+  timerView.render(questions);
+};
 const init = function () {
   questionView.addselectButtonhandler(controlQuestions);
+  controlQuestionNumber();
+  controltimer();
 };
 
 init();
