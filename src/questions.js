86ea415ae82f242dd0c257328a7970652e660273
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-04 14:28:13 +0530

    added questions

--- src/questions.js
+++ src/questions.js
@@ -1,16 +1,19 @@
 export const quizCompetitionstate = {
   questions: [
     {
+      questionNo: 1,
       question: "What is the result of the following expression: 5 + 3 * 2?",
       options: ["11", "16", "13", "9"],
       answer: "11",
     },
     {
+      questionNo: 2,
       question: "Which of the following is NOT a JavaScript data type?",
       options: ["String", "Number", "Boolean", "Float"],
       answer: "Float",
     },
     {
+      questionNo: 3,
       question:
         "What is the correct syntax to select an element with the id 'myElement' using JavaScript?",
       options: [
@@ -22,11 +25,13 @@ export const quizCompetitionstate = {
       answer: "document.getElementById('myElement')",
     },
     {
+      questionNo: 4,
       question: "What is the result of the following expression: '2' + 2?",
       options: ["22", "4", "TypeError", "NaN"],
       answer: "22",
     },
     {
+      questionNo: 5,
       question:
         "Which of the following is the correct way to declare a JavaScript array?",
       options: [
@@ -38,30 +43,35 @@ export const quizCompetitionstate = {
       answer: "var colors = [];",
     },
     {
+      questionNo: 6,
       question:
         "What does the 'typeof' operator in JavaScript return for the 'null' value?",
       options: ["object", "null", "undefined", "boolean"],
       answer: "object",
     },
     {
+      questionNo: 7,
       question:
         "Which method is used to add new elements to the end of an array in JavaScript?",
       options: ["push()", "unshift()", "concat()", "splice()"],
       answer: "push()",
     },
     {
+      questionNo: 8,
       question:
         "What is the output of the following code? console.log(3 + 2 + '7');",
       options: ["12", "327", "57", "37"],
       answer: "57",
     },
     {
+      questionNo: 9,
       question:
         "Which JavaScript method is used to remove the last element from an array?",
       options: ["pop()", "shift()", "slice()", "reduce()"],
       answer: "pop()",
     },
     {
+      questionNo: 10,
       question: "What does the '!=='' operator in JavaScript mean?",
       options: [
         "Not equal in value",
@@ -72,6 +82,7 @@ export const quizCompetitionstate = {
       answer: "Not equal in value and type",
     },
     {
+      questionNo: 11,
       question: "What is the purpose of the JavaScript 'map()' method?",
       options: [
         "Iterates over an array and applies a function to each element",
@@ -82,18 +93,21 @@ export const quizCompetitionstate = {
       answer: "Iterates over an array and applies a function to each element",
     },
     {
+      questionNo: 12,
       question:
         "What is the output of the following code? console.log(3 + 2 + '7');",
       options: ["12", "327", "57", "37"],
       answer: "57",
     },
     {
+      questionNo: 13,
       question:
         "Which JavaScript method is used to remove the last element from an array?",
       options: ["pop()", "shift()", "slice()", "reduce()"],
       answer: "pop()",
     },
     {
+      questionNo: 14,
       question: "What does the '!=='' operator in JavaScript mean?",
       options: [
         "Not equal in value",
@@ -104,6 +118,7 @@ export const quizCompetitionstate = {
       answer: "Not equal in value and type",
     },
     {
+      questionNo: 15,
       question: "What is the purpose of the JavaScript 'map()' method?",
       options: [
         "Iterates over an array and applies a function to each element",
@@ -114,11 +129,13 @@ export const quizCompetitionstate = {
       answer: "Iterates over an array and applies a function to each element",
     },
     {
+      questionNo: 16,
       question: "What does the '&&' operator in JavaScript represent?",
       options: ["Logical AND", "Logical OR", "Logical NOT", "Assignment"],
       answer: "Logical AND",
     },
     {
+      questionNo: 17,
       question:
         "Which method is used to convert a JavaScript object to a JSON string?",
       options: [
@@ -130,6 +147,7 @@ export const quizCompetitionstate = {
       answer: "JSON.stringify()",
     },
     {
+      questionNo: 18,
       question: "What is the purpose of the JavaScript 'forEach()' method?",
       options: [
         "Iterates over an array and applies a function to each element",
@@ -140,23 +158,27 @@ export const quizCompetitionstate = {
       answer: "Iterates over an array and applies a function to each element",
     },
     {
+      questionNo: 19,
       question:
         "Which built-in JavaScript method is used to round a number to the nearest integer?",
       options: ["Math.ceil()", "Math.floor()", "Math.round()", "Math.abs()"],
       answer: "Math.round()",
     },
     {
+      questionNo: 20,
       question: "What does the '||' operator in JavaScript represent?",
       options: ["Logical OR", "Logical AND", "Logical NOT", "Assignment"],
       answer: "Logical OR",
     },
     {
+      questionNo: 21,
       question:
         "Which method is used to check if a JavaScript string contains a specific substring?",
       options: ["includes()", "indexOf()", "startsWith()", "substring()"],
       answer: "includes()",
     },
     {
+      questionNo: 22,
       question: "What is the purpose of the JavaScript 'filter()' method?",
       options: [
         "Creates a new array with the results of calling a function on every element",
@@ -167,9 +189,28 @@ export const quizCompetitionstate = {
       answer: "Returns a new array containing elements that pass a condition",
     },
     {
+      questionNo: 23,
       question: "What is the result of the following code? typeof typeof 42;",
       options: ["number", "string", "undefined", "TypeError"],
       answer: "string",
+    },
+    {
+      questionNo: 24,
+      question: "Javascript is an _______ language?",
+      options: [
+        "Object-Oriented",
+        "Object-Based",
+        "Procedural",
+        "None of the above",
+      ],
+      answer: "Object-Oriented",
+    },
+    {
+      questionNo: 25,
+      question:
+        "Which of the following keywords is used to define a variable in Javascript?",
+      options: ["let", "const", "Both A and B", "None of the above"],
+      answer: "Both A and B",
     },
   ],
 };
