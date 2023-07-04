5faa0d01d2766558b3c50829a899057f41f86f09
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-04 14:29:06 +0530

    modified init function

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,14 @@
 import { quizCompetitionstate } from "./questions.js";
 import questionView from "./views/questionView.js";
-// import optionView from "./views/optionView.js";
-
-// console.log(quizCompetitionstate);
 
 const controlQuestions = function () {
   const { questions } = quizCompetitionstate;
+  console.log(questions);
   questionView.render(questions);
 };
 
-controlQuestions();
+const init = function () {
+  questionView.addselectButtonhandler(controlQuestions);
+};
 
-const init = function () {};
-questionView.addselectButtonhandler();
 init();
