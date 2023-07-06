c684460937e572012c6cba435f1a0a9d5803feff
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-06 11:22:07 +0530

    added controlscore function

--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,7 @@ import { quizCompetitionstate } from "./questions.js";
 import questionView from "./views/questionView.js";
 import questionNoView from "./views/questionNoView.js";
 import timerView from "./views/timerView.js";
+import scoreView from "./views/scoreView.js";
 
 const { questions } = quizCompetitionstate;
 const controlQuestions = function () {
@@ -16,9 +17,16 @@ const controlQuestionNumber = function () {
 const controltimer = function () {
   timerView.render(questions);
 };
+
+const controlScore = function () {
+  scoreView.render(questions);
+};
+
 const init = function () {
   questionView.addselectButtonhandler(controlQuestions);
+  // scoreView.updater();
   controlQuestionNumber();
+  controlScore();
   controltimer();
 };
 
