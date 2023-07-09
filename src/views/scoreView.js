00dff3d55cde012445e9e448cc47179defe85b57
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-09 19:59:12 +0530

    remove unwanted code

--- src/views/scoreView.js
+++ src/views/scoreView.js
@@ -4,18 +4,7 @@ class scoreView extends View {
   _parentEl = document.querySelector(".scoretextContainer");
 
   _generateMarkup() {
-    return `<p class="scoretext">SCORE : <span class="score">${this._scoreMarkup()}</span></p>`;
-  }
-
-  _scoreMarkup() {
-    let score = 0;
-    const updateScore = function () {
-      document.querySelector(".score").textContent = score;
-      score++;
-    };
-
-    setTimeout(updateScore, 0);
-    return score;
+    return `<p class="scoretext">SCORE : <span class="score">${0}</span></p>`;
   }
 }
 
