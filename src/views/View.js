47a97ef66d600c8c20ff57fb4d41d7be1f70a86b
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-10 16:29:35 +0530

    remove unwanted code

--- src/views/View.js
+++ src/views/View.js
@@ -11,33 +11,4 @@ export class View {
     this._parentEl.insertAdjacentHTML("afterbegin", markup);
     this._btnOption = document.querySelector(".options");
   }
-
-  // update(data, questions) {
-  //   this._data = data;
-  //   const newMarkup = this._generateMarkup();
-
-  //   const newDOM = document.createRange().createContextualFragment(newMarkup);
-  //   console.log(newDOM);
-  //   const newElements = Array.from(newDOM.querySelectorAll("*"));
-  //   console.log(newElements);
-
-  //   const curElement = Array.from(this._parentEl.querySelectorAll("*"));
-  //   console.log(Array.from(this._parentEl));
-  //   // newElements.forEach((newEl, i) => {
-  //   //   const curEl = curElement[i];
-
-  //   //   if (
-  //   //     !newEl.isEqualNode(curEl) &&
-  //   //     newEl.firstChild?.nodeValue.trim() !== ""
-  //   //   ) {
-  //   //     curEl.textContent = newEl.textContent;
-  //   //   }
-
-  //   //   if (!newEl.isEqualNode(curEl)) {
-  //   //     Array.from(newEl.attributes).forEach((attr) =>
-  //   //       curEl.setAttribute(attr.name, attr.value)
-  //   //     );
-  //   //   }
-  //   // });
-  // }
 }
