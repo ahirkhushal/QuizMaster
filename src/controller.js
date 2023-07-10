11715c0cfa59446c493e56cc4319d917f561e9ce
ahirkhushal <ahirkhushal559@gmail.com>
2023-07-10 16:28:39 +0530

    modified js code for reset timing

--- src/controller.js
+++ src/controller.js
@@ -3,9 +3,7 @@ import questionView from "./views/questionView.js";
 import questionNoView from "./views/questionNoView.js";
 import timerView from "./views/timerView.js";
 import scoreView from "./views/scoreView.js";
-// import nextQuestion from "./views/nextQuestion.js";
 
-// console.log(quizobj.quizCompetitionstate);
 const { questions } = quizobj.quizCompetitionstate;
 let questionNext;
 
@@ -13,9 +11,6 @@ const controlQuestions = function (question) {
   questionNext = question;
   questionView.render(question, questions);
   questionNoView.render(question, questions);
-};
-
-const controltimer = function (question) {
   timerView.render(question, questions);
 };
 
@@ -27,7 +22,6 @@ const init = function () {
   quizobj.addnextQueHandler(questions, controlQuestions);
   questionView.addselectButtonhandler(controlQuestions);
   controlScore(questionNext);
-  controltimer(questionNext);
 };
 
 init();
